db1e6a3196d5e4a895a61929c427290588d3d4fa
Elena Platonova <129943077+HelenePlatonova@users.noreply.github.com>
2023-06-04 13:36:27 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cdc89e0c-f5fd-4439-8160-4449a6f302fd.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits"
